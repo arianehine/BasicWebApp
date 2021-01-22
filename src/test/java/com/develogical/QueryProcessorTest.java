25738dc8c7594f9716c43f77e6930ce8e23ff72a
Ariane Hine <aah4@st-andrews.ac.uk>
2021-01-22 12:54:19 +0000

    3rd time lucky?

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class QueryProcessorTest {
 
@@ -27,7 +27,7 @@ public class QueryProcessorTest {
 
     @Test
     public void incorrectResponseFromAri() throws Exception {
-        assertThat(queryProcessor.process("ari"), containsString("no"));
+        assertFalse(queryProcessor.process("ari").contains("no"));
     }
 
     @Test
